86bf34679a05a207f539f78b7259dcd87c56f6f7
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2024-08-22 22:20:53 +0200

    Update urls.py

--- api/blog/urls.py
+++ api/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import BlogView, CategoryView
+from .views import BlogView, CategoryView, StatsView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog'),
     path('category/', CategoryView.as_view(), name='category'),
+    path('stats/', StatsView.as_view(), name='stats')
 ]
-
