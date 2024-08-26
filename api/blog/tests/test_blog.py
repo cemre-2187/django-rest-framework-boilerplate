775e70fdcc1d789953fe47eedf3cbccf63ec0466
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2024-08-26 22:09:40 +0200

    add stats test

--- api/blog/tests/test_blog.py
+++ api/blog/tests/test_blog.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from api.blog.models import Blog, Category
 from rest_framework.test import APIClient
 from api.core.tests.text_client import BaseTestClient
-from django.test import TestCase
 
 
 @pytest.mark.django_db
@@ -12,49 +11,49 @@ class TestBlogAPI(BaseTestClient):
   
     def test_get_blogs(self):
         
-        # Login For Test User
+        # Login for the test user
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_access_token)
         
-        # Test bloğunu oluşturma
+        # Create a test blog
         Blog.objects.create(title="Test Blog", content="This is a test blog content.", author=self.test_user, category=self.test_category)
 
-        # GET isteği gönderme
+        # Send a GET request
         response = self.client.get(reverse('blog'))
 
-        # # İsteğin başarılı olup olmadığını kontrol etme
+        # Check if the request was successful
         assert response.status_code == 200
         assert len(response.data['data']) == 2
         assert response.data['data'][0]['title'] == "Test Blog"
 
     def test_create_blog(self):
-        # Login For  User
+        # Login for the user
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_access_token)
         
-        # POST isteği gönderme
+        # Send a POST request
         response = self.client.post(reverse('blog'), self.blog_data)
 
-        # Blogun başarılı bir şekilde oluşturulup oluşturulmadığını kontrol etme
+        # Check if the blog was created successfully
         assert response.status_code == 200
         assert Blog.objects.count() == 2
-        # get blog with title filter
+        # Get blog with title filter
         assert Blog.objects.filter(title="Create Test Blog").exists()
 
     def test_create_blog_unauthorized(self):
-        # Giriş yapmadan POST isteği gönderme
+        # Send a POST request without logging in
         response = self.client.post(reverse('blog'), self.blog_data)
 
-        # Blog oluşturma isteğinin yetkisiz olup olmadığını kontrol etme
+        # Check if the blog creation request was unauthorized
         assert response.status_code == 401
         assert Blog.objects.count() == 1
 
     def test_create_blog_invalid_author(self, client, test_user, blog_data):
-        # Login For  User
+        # Login for the user
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_access_token)
 
-        # Farklı bir yazar ile blog oluşturma denemesi
+        # Attempt to create a blog with a different author
         blog_data['author'] = test_user.id + 1
         response = client.post(reverse('blog'), blog_data)
 
-        # Blog oluşturma isteğinin yetkisiz olup olmadığını kontrol etme
+        # Check if the blog creation request was unauthorized
         assert response.status_code == 401
         assert Blog.objects.count() == 1
--- api/blog/tests/test_stats.py
+++ api/blog/tests/test_stats.py
@@ -1,32 +1,20 @@
-# import pytest
-# from django.urls import reverse
-# from django.contrib.auth.models import User
-# from rest_framework.test import APIClient
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from api.core.tests.text_client import BaseTestClient
 
-# @pytest.fixture
-# def test_user(db):
-#     return User.objects.create_user(username='testuser', password='testpass')
+@pytest.mark.django_db
+class TestStatsAPI(BaseTestClient):
 
-
-# @pytest.fixture
-# def get_access_token(client, test_user):
-#     url = '/account/login/'
-#     response = client.post(url, {'username': 'testuser', 'password': 'testpass'})
-#     assert response.status_code == 200
-#     print(response.data)
-#     return response.data['data']['access']
-
-# @pytest.mark.django_db
-# class TestStatsAPI:
-
-#     def test_get_stats(self, client,get_access_token):
-#         client=APIClient()
-#         client.credentials(HTTP_AUTHORIZATION='Bearer ' + get_access_token)
+    def test_get_stats(self):
+        # Login for the test user
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.get_access_token)
   
-#         # GET isteği gönderme
-#         response = client.get(reverse('stats'))
+        # Send a GET request
+        response = self.client.get(reverse('stats'))
 
-#         # İsteğin başarılı olup olmadığını kontrol etme
-#         assert response.status_code == 200
-#         assert 'data' in response.data
-#         assert response.data['message'] == "Stats fetched successfully"
+        # Check if the request was successful
+        assert response.status_code == 200
+        assert 'data' in response.data
+        assert response.data['message'] == "Stats fetched successfully"
