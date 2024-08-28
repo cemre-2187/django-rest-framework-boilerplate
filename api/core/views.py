2f759f34a7e8497bf513360acd1a79297993a48b
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2024-08-28 17:16:22 +0200

    add middleware

--- api/core/views.py
+++ api/core/views.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from api.core.mixins import APIViewResponseMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
 
 class BaseAPIView(APIView, APIViewResponseMixin):
     """
-    Create a base API view that combines APIView and APIViewResponseMixin
-    Custom API View to handle all the common logic for all APIs
-    """  
-    def get_authenticate_header(self, request):
-        return super().get_authenticate_header(request)
+    Custom base API view to handle common logic for all APIs.
+    """
+    # Add custom middleware logic to here for all API views
+    def dispatch(self, request, *args, **kwargs):
+        # Custom middleware logic can be added here
+        print("Custom middleware logic for all API views If you need it", "/api/core/views.py")
+        return super().dispatch(request, *args, **kwargs)
+    
     pass
