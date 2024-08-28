112d573d4b219d43e82e0be4bb906854b3dc0858
cemre-2187 <55685979+cemre-2187@users.noreply.github.com>
2024-08-28 17:43:08 +0200

    add swagger urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documantation",
+        default_version='v1',
+        description="API endpoints for the blog application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="info@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('account/', include('api.account.urls'), name='account'),
     path('blog/', include('api.blog.urls'), name='blog'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
